cb5e3bdd8640452d580a100a649f2f85af52ea95
Vitali_Vianhura <vengura.code@gmail.com>
2020-10-22 22:44:37 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openings = {};
+    const closings = {};
+
+    for (const config of bracketsConfig) {
+        const [open, close] = config;
+        openings[open] = close;
+        closings[close] = open;
+    }
+
+    const stack = [];
+
+    for(let i = 0; i < str.length; i++) {
+        const char = str.charAt(i);
+
+        const close = openings[char];
+        const open = closings[char];
+
+        if ((open !== close && Boolean(close)) || (open === close && !stack.includes(char))) {
+            stack.push(char);
+        } else {
+            const last = stack.pop();
+            if (open !== last) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
